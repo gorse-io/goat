e98b53894560ef56b4f0374519a7bb83cdff10e7
zhenghaoz <zhangzhenghao@hotmail.com>
2025-03-24 20:27:26 +0800

    support typed arguments (#23)

--- tests/universal_test.go
+++ tests/universal_test.go
@@ -10,14 +10,14 @@ import (
 func TestAdd(t *testing.T) {
 	a := int64(1)
 	b := int64(2)
-	c := add(unsafe.Pointer(uintptr(a)), unsafe.Pointer(uintptr(b)))
+	c := add(a, b)
 	assert.Equal(t, a+b, c)
 }
 
 func TestL2(t *testing.T) {
 	a := []float32{1, 2, 3, 4}
 	b := []float32{5, 6, 7, 8}
-	c := l2(unsafe.Pointer(&a[0]), unsafe.Pointer(&b[0]), unsafe.Pointer(uintptr(len(a))))
+	c := l2(unsafe.Pointer(&a[0]), unsafe.Pointer(&b[0]), int64(len(a)))
 	assert.Equal(t, float32(64), c)
 }
 
@@ -25,6 +25,6 @@ func TestMatMul(t *testing.T) {
 	a := []float32{1, 2, 3, 4}
 	b := []float32{5, 6, 7, 8}
 	res := make([]float32, 4)
-	mat_mul(unsafe.Pointer(&a[0]), unsafe.Pointer(&b[0]), unsafe.Pointer(&res[0]), unsafe.Pointer(uintptr(2)), unsafe.Pointer(uintptr(2)), unsafe.Pointer(uintptr(2)))
+	mat_mul(unsafe.Pointer(&a[0]), unsafe.Pointer(&b[0]), unsafe.Pointer(&res[0]), 2, 2, 2)
 	assert.Equal(t, []float32{19, 22, 43, 50}, res)
 }
